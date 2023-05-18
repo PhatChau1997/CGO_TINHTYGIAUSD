01654cab446f5e9a47486fe135a3c00002891d5d
PhatChau1997 <133665829+PhatChau1997@users.noreply.github.com>
2023-05-18 12:54:59 +0700

    Initial commit

---
+++ CGO_TINHTYGIAUSD/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_TINHTYGIAUSD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float usd;
+            int a;
+            a = 23000;
+                       Console.Write("nhap so usd muon doi :");
+            usd = float.Parse(Console.ReadLine());
+
+            float vnd = usd * a;
+            Console.WriteLine("so tien vnd cua ban la:" + vnd ); 
+            Console.ReadLine();
+        }
+    }
+}
